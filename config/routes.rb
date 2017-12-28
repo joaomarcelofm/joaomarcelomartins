22f8c9499665c11b7d1b1215ef6b95cbf47014e6
Joao Martins <joaomarcelofm@gmail.com>
2017-12-28 16:19:54 -0200

    Initial set up for Project Page + Routes Projects Show added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/management', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
+
+  resources :projects, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
