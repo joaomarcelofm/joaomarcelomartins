75712172ff5b787542a851bd8ea3f4c014857fea
Joao Martins <joaomarcelofm@gmail.com>
2017-12-29 15:30:27 -0200

    Configured BlogPosts routing for SHOW pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :projects, only: [:show]
+  resources :blog_posts, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
