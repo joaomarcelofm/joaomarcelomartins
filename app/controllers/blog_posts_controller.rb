89f2638369e06516b46b2c726e7b9019e9e7ea8d
Joao Martins <joaomarcelofm@gmail.com>
2017-12-29 15:54:51 -0200

    Blog Post Show styling done

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,2 +1,12 @@
 class BlogPostsController < ApplicationController
+  before_action :set_blog_post, only: [:show]
+
+  def show
+  end
+
+  private
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  end
 end
