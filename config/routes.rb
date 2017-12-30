a6e37c6b16ebd6df577314198390bc43c7f02765
Joao Martins <joaomarcelofm@gmail.com>
2017-12-30 11:22:13 -0200

    Changed devise settings to allow single user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/management', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations"}
   root to: 'pages#home'
 
   resources :projects, only: [:show]
