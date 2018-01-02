e2e45d1268a9f164bbd7bb6ca1c1d384dc187bd5
Joao Martins <joaomarcelofm@gmail.com>
2018-01-02 19:44:29 -0200

    Fixed access rights to show pages

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,4 +1,5 @@
 class BlogPostsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
   before_action :set_blog_post, only: [:show]
 
   def show
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
   before_action :set_project, only: [:show]
 
   def index
