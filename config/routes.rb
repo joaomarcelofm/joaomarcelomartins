f224b945ac39d19da7085e99fb6d3a6bf60bd941
Joao Martins <joaomarcelofm@gmail.com>
2018-01-04 15:08:48 -0200

    Fixed URL display for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :projects, only: [:show]
-  resources :blog_posts, only: [:show]
+  resources :blog_posts, only: [:show], path: 'blog'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
