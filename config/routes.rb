ba9bee6b2c014738f1236c0d12751a4ac4b0c8ea
Joao Martins <joaomarcelofm@gmail.com>
2018-01-04 15:20:59 -0200

    Fixed login url to management interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/management', as: 'rails_admin'
-  devise_for :users, controllers: { registrations: "registrations"}
+  devise_for :users, controllers: { registrations: "registrations"},  path: '', path_names: { sign_in: 'login-manangement', sign_out: 'logout'}
+
   root to: 'pages#home'
 
   resources :projects, only: [:show]
