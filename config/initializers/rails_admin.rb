8b8406a25aa305fc87ee52cdcfe17934a793858d
Joao Martins <joaomarcelofm@gmail.com>
2018-03-07 07:52:45 +0100

    Admin routes updated

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -23,19 +23,6 @@ RailsAdmin.config do |config|
   ## To disable Gravatar integration in Navigation Bar set to false
   # config.show_gravatar = true
 
-  RailsAdmin.config do |config|
-    config.authorize_with do |controller|
-      redirect_to main_app.root_path unless current_user && current_user.admin
-    end
-
-    # [...]
-  end
-
-  # logout option
-  config.current_user_method(&:current_user)
-
-  #
-
   config.actions do
     dashboard                     # mandatory
     index                         # mandatory
@@ -51,41 +38,4 @@ RailsAdmin.config do |config|
     # history_index
     # history_show
   end
-
-  # Config settings for Project Model
-
-  RailsAdmin.config do |config|
-    config.model Project do
-      edit do
-        field :name
-        field :client
-        field :description
-        field :thumbnail
-        field :main_image
-        field :full_description, :wysihtml5 do
-          config_options toolbar: { fa: true }, # use font-awesome instead of glyphicon
-                         html: true, # enables html editor
-                         parserRules: { tags: { p:1 } } # support for <p> in html mode
-        end
-      end
-    end
-  end
-
-  # Config settings for BlogPosts Model
-
-  RailsAdmin.config do |config|
-    config.model BlogPost do
-      edit do
-        field :title
-        field :intro
-        field :banner_image
-        field :content, :wysihtml5 do
-          config_options toolbar: { fa: true }, # use font-awesome instead of glyphicon
-                         html: true, # enables html editor
-                         parserRules: { tags: { p:1 } } # support for <p> in html mode
-        end
-      end
-    end
-  end
-
 end
